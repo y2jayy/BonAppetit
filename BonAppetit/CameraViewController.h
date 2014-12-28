8aae7537b6fb9a39500a6cfd296adea9da97adb4
Jay Yoon <jyoon@skycareehr.com>
2014-12-28 13:39:02 -0800

    camera view mockup created and stars view files added to project

---
+++ BonAppetit/CameraViewController.h
@@ -0,0 +1,19 @@
+//
+//  CameraViewController.h
+//  BonAppetit
+//
+//  Created by Jay Yoon on 12/27/14.
+//  Copyright (c) 2014 Jay Yoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+- (IBAction)takePhoto:(UIButton *)sender;
+- (IBAction)selectPhoto:(UIButton *)sender;
+
+
+@end
