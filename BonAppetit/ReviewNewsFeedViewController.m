1e72502d6f8531ee41b27f021065ba9ea54f138f
Jay Yoon <jyoon@skycareehr.com>
2015-01-08 19:22:06 -0800

    food feed image and rating showing

--- BonAppetit/ReviewNewsFeedViewController.m
+++ BonAppetit/ReviewNewsFeedViewController.m
@@ -9,6 +9,7 @@
 #import "ReviewNewsFeedViewController.h"
 #import "Masonry/Masonry.h"
 #import "StrechyParallaxScrollView.h"
+#import "ASStarRatingView.h"
 
 #define RGBCOLOR(r,g,b)     [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1]
 
@@ -69,28 +70,67 @@
     float itemStartY = topView.frame.size.height + 10;
     for (int i = 1; i <= 10; i++) {
         [strechy addSubview:[self scrollViewItemWithY:itemStartY andNumber:i]];
-        itemStartY += 190;
+        itemStartY += 360;
     }
     
     //set scrollable area (classic uiscrollview stuff)
     [strechy setContentSize:CGSizeMake(width, itemStartY)];
 }
 
-- (UILabel *)scrollViewItemWithY:(CGFloat)y andNumber:(int)num {
-    UILabel *item = [[UILabel alloc] initWithFrame:CGRectMake(10, y, [UIScreen mainScreen].bounds.size.width-20, 180)];
+- (UIView *)scrollViewItemWithY:(CGFloat)y andNumber:(int)num {
+    UIView *item = [[UIView alloc] initWithFrame:CGRectMake(10, y, [UIScreen mainScreen].bounds.size.width-20, 350)];
     [item setBackgroundColor:[self randomColor]];
-    [item setText:[NSString stringWithFormat:@"Item %d", num]];
-    [item setTextAlignment:NSTextAlignmentCenter];
-    [item setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:26]];
-    [item setTextColor:[UIColor whiteColor]];
+    
+    UIImageView *reviewImageView = [[UIImageView alloc] initWithFrame:CGRectMake(item.superview.bounds.origin.x + 10, item.superview.bounds.origin.y + 10, item.bounds.size.width - 20, item.bounds.size.width - 20)];
+    [reviewImageView setImage:[UIImage imageNamed:[NSString stringWithFormat:@"food%d.jpg", num]]];
+    
+    //testing
+    ASStarRatingView *staticStarRatingView = [[ASStarRatingView alloc] initWithFrame:CGRectMake(item.superview.bounds.origin.x + 10, item.bounds.origin.y + 250, item.bounds.size.width - 20, (item.bounds.size.width - 20) / 2)];
+    staticStarRatingView.canEdit = NO;
+    staticStarRatingView.maxRating = 5;
+    staticStarRatingView.rating = [self randomRating];
+    //testing
+    
+    [item addSubview:reviewImageView];
+    [item bringSubviewToFront:reviewImageView];
+    
+    //testing
+    [item addSubview:staticStarRatingView];
+    [item bringSubviewToFront:staticStarRatingView];
+    //testing
+    
+//    [item setText:[NSString stringWithFormat:@"Item %d", num]];
+//    [item setTextAlignment:NSTextAlignmentCenter];
+//    [item setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:26]];
+//    [item setTextColor:[UIColor whiteColor]];
     return item;
 }
+
+//- (UIView *)scrollViewItemWithY:(CGFloat)y andNumber:(int)num {
+//    UIView *item = [[UIView alloc] initWithFrame:CGRectMake(5, y, [UIScreen mainScreen].bounds.size.width-10, 185)];
+//    UIImageView *reviewImageView = [[UIImageView alloc] initWithFrame:CGRectMake(item.frame.origin.x + 5, y + 5, item.frame.size.height - 10, item.frame.size.width - 10)];
+//    [reviewImageView setImage:[UIImage imageNamed:@"page4.png"]];
+//    [reviewImageView setContentMode:UIViewContentModeScaleAspectFit];
+//    [item addSubview:reviewImageView];
+//    
+//    [item setBackgroundColor:[self randomColor]];
+////    [item setText:[NSString stringWithFormat:@"Item %d", num]];
+////    [item setTextAlignment:NSTextAlignmentCenter];
+////    [item setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:26]];
+////    [item setTextColor:[UIColor whiteColor]];
+//    return item;
+//}
 
 - (UIColor *)randomColor {
     CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
     CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
     CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
     return [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+}
+
+- (int)randomRating {
+    int rating = ( arc4random() % 5 + 1 );  //  0.0 to 1.0
+    return rating;
 }
 
 - (void)didReceiveMemoryWarning {
